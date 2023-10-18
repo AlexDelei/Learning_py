0892c87e6fc7b6fda894b7212b97a28e336aadbc
AlexDelei <alexdelei125@gmail.com>
2023-10-18 22:27:42 +0300

    operators

--- operators.py
+++ operators.py
@@ -37,6 +37,8 @@ Operator    Example    Same as
   >>=       x >>= 3     x = x >> 3
   <<=       x <<= 3     x = x << 3
 
+
+
 3. Comparison operators
 Compare two values
 Operator    Name
@@ -60,6 +62,30 @@ Operator        Description                                             Example
  is             Returns true if both variables are the same object      x is y
  is not         Returns true if both variables are not the same objects  x is not y
 
+6. Membership Operators
+Are used to test if a sequence are represented in an object
+Operator    Description                                           Example_in_use
+  in        Returns True if a sequence with the specified value   x in y
+            is present in the object
+  not in    Returns True if a sequence with the specified value   x not in y
+            is not present in an object
+
+7.Python Bitwise operators
+Are used to compare 'binary numbers'
+Operator    Name      Description                       Example
+  &         AND       Sets each bit to 1 if both bits    x & y
+                      are 1
+  |         OR        sets each bit to 1 if one of both  x | y
+                      bits  are 1
+  ^         XOR       sets each bit to 1 if only one of  x ^ y
+                      two bits is 1
+  ~         NOT       inverts all bits                    ~x
+  <<       Zero fill   shift left by pushing zeros in      x << 2
+           left shift  from the right and let the leftmost
+                       bits fall off
+  >>      signed right  shifts right by pushing copies of   x >> 2
+          shift               the leftmost bit in from the left,
+                        and let the rightmost bits falloff
 """
 x = ["bananas", "apple"]
 y = ["bananas", "apple"]
@@ -72,3 +98,32 @@ print(x is y)
 print(x == y)
 #prints True since this compares the two values . This brings the difference be2wn
 #'is' and '=='
+
+a = 1
+a <<= 1
+print(a)
+
+#membership operators
+x = ["banana", "pineapple"]
+print("pineapple" in x)
+"""
+#operator precedence
+this describes the order in which the operations are performed i.e from highly prioritized
+
+This is the order , from highly to lowly:
+Operator
+
+()
+**
++x, -x, ~x
+* / // %
++ , -
+>> , <<
+&
+^
+|
+==  !=  >  >=  <  <=  is  is not  in  not in 
+not(logical not)
+and(logical and)
+or(logical or)
+"""
